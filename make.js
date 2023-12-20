e114200e81ed7cb3b92fde83f694d2b68824177d
Taisuke Fukuno <fukuno@jig.jp>
2023-12-20 16:57:39 +0900

    add icons.csv and the builder script

---
+++ make.js
@@ -0,0 +1,5 @@
+import { dir2array } from "https://js.sabae.cc/dir2array.js";
+import { CSV } from "https://js.sabae.cc/CSV.js";
+
+const fns = await dir2array("./icons/");
+await Deno.writeTextFile("./icons.csv", CSV.stringify(fns.filter(i => i.endsWith(".svg")).map(i => ({ name: i.substring(0, i.indexOf(".")), icon: "icons/" + i }))));
